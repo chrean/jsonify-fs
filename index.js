8d1acc8696e5ca5c481434f4491b51ee72c40a07
Gabriel Sambarino <gabriel.sambarino@gmail.com>
2018-05-16 15:41:09 +0200

    Cleanup

--- index.js
+++ index.js
@@ -28,46 +28,11 @@ program
   .option('-f, --format <JSON/XML>', 'Output format')
   .action( ( directory, options ) => {
 
-    //console.log( options.format );
-
-    //let fs = require('fs');
-    //let files = fs.readdirSync(directory);
-    //console.log('format: %s directory: %s',
-    //var fileTree = [];
-
     const myList = listFiles( directory );
 
     console.log( myList );
     
-    //var myJSON = JSON.parse(files);
-    //console.log(JSON.stringify(fileTree, null, 2));
-    //console.log( files );
-
     }
   );
 
 program.parse( process.argv );
-
-
-
-//var cmds = program.commands.map( c => c._name );
-
-
-
-/*
-.action(function(directory) {
-  var fs = require('fs');
-  var files = fs.readdirSync(directory);
-  //console.log('format: %s directory: %s',
-  //  program.format, directory);
-  var fileTree = [];
-  files.forEach ( function (arrayItem) {
-    var fileItem = new Object();
-    fileItem.filename = arrayItem;
-    fileTree.push( JSON.stringify(fileItem) );
-  });
-  var myJSON = JSON.parse(fileTree);
-  console.log(JSON.stringify(fileTree));
-
-}
-*/
